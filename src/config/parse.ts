a7130342076a5cbd5f738e3828bd6a7e8bda5445
faisal07m <faisal07m@gmail.com>
2024-03-12 09:57:18 +0100

    update

--- src/config/parse.ts
+++ src/config/parse.ts
@@ -1,11 +1,11 @@
 export const APP_ID = "what2study";
 export const KEY = "";
-export const SERVER_URL= "http://localhost:1349/what2study/parse";
-export const SERVER_URL_ ="https://www.cpstech.de/what2study/parse";
+export const SERVER_URL_= "http://localhost:1349/what2study/parse";
+export const SERVER_URL ="https://www.cpstech.de/what2study/parse";
 
-export const SERVER_URL_parsefunctions ="http://localhost:1349/what2study/parse/functions";
+export const SERVER_URL_parsefunctions_ ="http://localhost:1349/what2study/parse/functions";
 
-export const SERVER_URL_parsefunctions_ ="https://www.cpstech.de/functions";
+export const SERVER_URL_parsefunctions ="https://www.cpstech.de/functions";
 export const yellowMarkerBase64 = "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-gold.png"
 export const blackMarkerBase64 ="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-black.png"
 export const blueMarkerBase64 ="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png"
